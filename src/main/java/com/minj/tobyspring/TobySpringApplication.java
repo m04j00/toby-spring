3b74b47baca0686a8cb736ef5e47287c8ffe6cae
MinJ <m04j00@gmail.com>
2022-07-28 11:18:28 +0900

    setting : 초기 커밋

---
+++ src/main/java/com/minj/tobyspring/TobySpringApplication.java
@@ -0,0 +1,13 @@
+package com.minj.tobyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minj/tobyspring/TobySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minj.tobyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
