1b4e039a1dd204c15b7093b53eb6393e1c1fc91c
MinJ <m04j00@gmail.com>
2022-08-01 11:55:57 +0900

    #1 1.3.1 클래스 분리
    1. SimpleConnectionMaker 클래스를 생성해 새 DB 생성 기능을 구현한다.
    2. UserDao에서 new 키워드를 사용해 클래스의 오브젝트를 만든다.
    3. add(), get() 메소드에서 DB 커넥션을 가져온다.

--- src/main/java/com/minj/tobyspring/TobySpringApplication.java
+++ src/main/java/com/minj/tobyspring/TobySpringApplication.java
@@ -1,13 +1,28 @@
 package com.minj.tobyspring;
 
+import com.minj.tobyspring.user.dao.UserDao;
+import com.minj.tobyspring.user.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class TobySpringApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
         SpringApplication.run(TobySpringApplication.class, args);
+        UserDao dao = new UserDao();
+        User user = new User();
+        user.setId("m772");
+        user.setName("이민지");
+        user.setPassword("doremisolsolfa");
+        dao.add(user);
+        System.out.println(user.getId() + " 등록 성공");
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + " 조회 성공");
     }
 
 }
---
+++ src/main/java/com/minj/tobyspring/user/dao/SimpleConnectionMaker.java
@@ -0,0 +1,16 @@
+package com.minj.tobyspring.user.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleConnectionMaker {
+    public Connection makeNewConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+        Connection c = DriverManager.getConnection(
+                "jdbc:mysql://localhost/toby", "spring", "book");
+
+        return c;
+    }
+}
