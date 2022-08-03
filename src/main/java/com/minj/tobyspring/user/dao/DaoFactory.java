eb250b225181060ba1d41d8ecc7764e4766a7e92
Min J <m04j00@gmail.com>
2022-08-03 22:04:07 +0900

    #8 1.5.1 오브젝트 팩토리를 이용한 스프링 IoC

--- src/main/java/com/minj/tobyspring/user/dao/DaoFactory.java
+++ src/main/java/com/minj/tobyspring/user/dao/DaoFactory.java
@@ -1,15 +1,16 @@
 package com.minj.tobyspring.user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker());
     }
-    public AccountDao accountDao(){
-        return new AccountDao(connectionMaker());
-    }
-    public MessageDao messageDao(){
-        return new MessageDao(connectionMaker());
-    }
+
+    @Bean
     public ConnectionMaker connectionMaker(){
         return new KConnectionMaker();
     }
--- src/main/java/com/minj/tobyspring/user/dao/UserDaoTest.java
+++ src/main/java/com/minj/tobyspring/user/dao/UserDaoTest.java
@@ -1,13 +1,21 @@
 package com.minj.tobyspring.user.dao;
 
 import com.minj.tobyspring.user.domain.User;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        UserDao dao = new DaoFactory().userDao();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        // 파라미터 `userDao`는 `ApplicationContext에 등록된 빈의 이름이다.
+        // `DaoFactory`에서 `@Bean` 어노테이션을 `userDao` 이름의 메소드에 붙였는데,
+        // 메소드 이름이 자동으로 빈의 이름이 된다.
+        // `userDao`라는 이름의 빈을 가져온다는 것은 `DaoFactory`의 `userDao()` 메소드를 호출해서
+        // 그 결과를 가져온다고 생각하면 된다.
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("m772");
